dacdf551168ba965f097ef76c7276050744987d2
ShivangiChotaliya <shivangichotaliya89@gmail.com>
2021-03-10 23:54:27 +0530

    Adding Doctor Record Successfull

--- ServerClient/Client.java
+++ ServerClient/Client.java
@@ -6,24 +6,31 @@ import java.nio.charset.StandardCharsets;
 import java.rmi.*;
 import java.util.*;
 
+import Models.DoctorModel;
+
 public class Client {
     public static void main(String[] args)throws Exception
     {
         BufferedReader inFromUSer = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket clientSocket = new DatagramSocket();
-        byte[] recieveData = new byte[1024];
-        byte[] sendData = new byte[1024];
         String sentance = "";
         while(!sentance.equals("bye"))
         {
-            RemoteStub stub = (RemoteStub) Naming.lookup("rmi://192.168.1.1:5000/mcaiv");
+            RemoteStub stub = (RemoteStub) Naming.lookup("rmi://localhost:5000/mcaiv");
+            
+            DoctorModel doctor = new DoctorModel();
+            System.out.println("Enter doctorname : ");
+            doctor.setName(inFromUSer.readLine());
+            System.out.println("Enter Speciality : ");
+            doctor.setspeciality(inFromUSer.readLine());
+            System.out.println("Enter Gender : ");
+            doctor.setgender(inFromUSer.readLine());
+            System.out.println("Enter Phoneno : ");
+            doctor.setphoneno(inFromUSer.readLine());
+            System.out.println("Enter Address : ");
+            doctor.setaddress(inFromUSer.readLine());
+            stub.SetDoctor(doctor);
+            System.out.println("Want to continue:");
             sentance = inFromUSer.readLine();
-            stub.test(sentance);
-            // InetAddress Address = InetAddress.getLocalHost();
-            // sendData = sentance.getBytes();
-            // DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,Address,9876);
-            // clientSocket.send(sendPacket);
         }
-        clientSocket.close();
     }
 }
